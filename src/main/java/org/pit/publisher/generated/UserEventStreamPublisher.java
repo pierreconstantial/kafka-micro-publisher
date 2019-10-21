156e803bbb6df1d8502eecf531b0442381f8b48f
pierre <pierre.constantial@upstream-mobility.at>
2019-10-21 14:34:42 +0200

    Don't use optional in place of if/else

--- src/main/java/org/pit/publisher/generated/UserEventStreamPublisher.java
+++ src/main/java/org/pit/publisher/generated/UserEventStreamPublisher.java
@@ -8,6 +8,7 @@ import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Component;
+import org.springframework.util.ObjectUtils;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -17,8 +18,6 @@ import java.util.Optional;
 @Component
 @Slf4j
 public class UserEventStreamPublisher {
-
-
 
   @Autowired
   private UserEventStreamSource userEventStreamSource;
@@ -48,9 +47,7 @@ public class UserEventStreamPublisher {
   }
 
   private Map<String, String> headerMap(Map<String, String> headers, String type, String partitionKey) {
-    Map<String, String> map = Optional
-      .ofNullable(headers)
-      .orElse(new HashMap<>());
+    Map<String, String> map = headers == null ? new HashMap<>() : headers;
     map.put("type", type);
     map.put("partitionKey", partitionKey);
     return map;
